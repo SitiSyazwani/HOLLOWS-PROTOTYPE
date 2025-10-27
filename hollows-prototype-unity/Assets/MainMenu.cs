a7bc24ca7ae09c867310b7ab8dcc3362f3ed32dd
Qayzan <50222777+Qayzan@users.noreply.github.com>
2025-10-27 11:13:59 +0800

    Updated Menu with nicer highlighted animation with fade effect

--- hollows-prototype-unity/Assets/MainMenu.cs
+++ hollows-prototype-unity/Assets/MainMenu.cs
@@ -1,12 +1,38 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private Image fadePanel;
+    [SerializeField] private float fadeDuration = 1f;
+
     public void PlayGame()
     {
+        StartCoroutine(FadeAndLoadScene());
+    }
+
+    private IEnumerator FadeAndLoadScene()
+    {
+        if (fadePanel != null)
+        {
+            Color startColor = fadePanel.color;
+            Color targetColor = new Color(0f, 0f, 0f, 1f);
+            
+            float elapsed = 0f;
+            
+            while (elapsed < fadeDuration)
+            {
+                elapsed += Time.deltaTime;
+                fadePanel.color = Color.Lerp(startColor, targetColor, elapsed / fadeDuration);
+                yield return null;
+            }
+            
+            fadePanel.color = targetColor;
+            yield return new WaitForSeconds(0.2f);
+        }
+        
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
